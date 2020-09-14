4ea55cfe8d3a11e759bbf315a06049091bbaf689
dhmec <dharmoradiya@gmail.com>
2020-09-14 15:59:10 -0600

    add component and link with index.js

---
+++ src/components/Calculator.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+
+
+const Calculator = props => {
+    
+    
+    const [firstInput, setFirstInput] = useState( '' );
+    const [secondInput, setSecondInput] = useState( '' );
+    const [newOption, setNewOption] = useState( 'addition' );
+    const [result, setResult]= useState( '' );
+    const firstInputNumber = parseFloat(firstInput);
+    const secondInputNumber = parseFloat(secondInput);
+    
+    
+
+    // submit event function
+    const displayCalculate  = event => {
+        event.preventDefault();       
+        selectOpearator();                
+    } 
+    // select operator
+    const selectOpearator = () => 
+    {
+        if (newOption ==='addition')
+        {
+            setResult(firstInputNumber + secondInputNumber);            
+        }
+        else if (newOption === 'substraction')
+        {
+            setResult(firstInputNumber - secondInputNumber);            
+        }
+        else if (newOption === 'multiplication')
+        {
+            setResult(firstInputNumber * secondInputNumber);            
+        }
+        else 
+        {
+            setResult(firstInputNumber / secondInputNumber);            
+        }
+    } 
+    
+    //Retrun(JSX) 
+    return (
+      
+        <div>
+            <h1>
+                Welcome to my Calculator!
+            </h1> 
+            <form onSubmit={displayCalculate}>
+                <label htmlFor="firstInput">Input 1:</label>
+                <input type="text" 
+                       id="firstInput"
+                       onChange={e => { setFirstInput( e.target.value ) }}
+                       value={firstInput} />
+                
+                <label htmlFor="selectBox"> Operation:</label>
+                <select id="selectBox" onChange={e => {setNewOption (e.target.value)}} value={newOption} htmlFor="selectBox">
+                        <option value="addition">+ (Add)</option>
+                        <option value="substraction">- (Sub)</option>
+                        <option value="multiplication">* (Mul)</option>
+                        <option value="division">/ (Div)</option>
+                </select>
+
+                <label htmlFor="secondInput">Input 2:</label>
+                <input type="text" 
+                       id="secondInput"
+                       onChange={e => { setSecondInput( e.target.value ) }}
+                       value={secondInput} />
+
+                <input type='submit' value="Calculate!" />
+            </form>
+            <p>Result : {result} </p>
+        </div>
+      
+    );
+}
+  
+export default Calculator
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './components/Calculator.css';
+import Calculator from './components/Calculator';
+import Singleinputcalculator from './components/Singleinputcalculator';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <> </>
+    <Calculator />
+    <Singleinputcalculator />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
