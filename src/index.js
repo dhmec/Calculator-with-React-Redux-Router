1564f72916efe65a26ded0a2342953848aef17b7
dhmec <dharmoradiya@gmail.com>
2020-09-14 16:10:38 -0600

    installed redux and react-redux

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import './components/Calculator.css';
 import Calculator from './components/Calculator';
-import Singleinputcalculator from './components/Singleinputcalculator';
+
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Calculator />
-    <Singleinputcalculator />
+    <Calculator />    
   </React.StrictMode>,
   document.getElementById('root')
 );
